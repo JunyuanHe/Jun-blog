b87ef3654d47016c585cf30014baf6940205ad7d
Junyuan He <hejunyuan01@126.com>
2025-05-26 20:56:17 +0800

    Test deployment

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/Jun-blog/',
   lang: 'zh-CN',
   locales: {
     '/': {
